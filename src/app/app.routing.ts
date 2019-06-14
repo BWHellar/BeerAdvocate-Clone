cbf43c00d67f57326a004962f5c32f0425deffa0
bh <bwhellar@gmail.com>
2019-06-14 14:24:28 -0700

    work on bottom bar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,17 +2,23 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { CenterComponent } from './center/center.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 
 const appRoutes: Routes = [
   {
   path: '',
   component: TopBarComponent
-},
+  },
   {
   path: "",
   component: CenterComponent,
   outlet: "center"
+  },
+  {
+    path: "",
+    component: BottomBarComponent,
+    outlet: "bottom"
   }
 ];
 
