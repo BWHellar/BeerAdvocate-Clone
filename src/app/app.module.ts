bc6ddd209e6b3b648693676417c5ff0e6a8a2ceb
bh <bwhellar@gmail.com>
2019-06-14 12:30:59 -0700

    disperse components to their correct area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
+import { CenterComponent } from './center/center.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    BottomBarComponent,
+    CenterComponent
   ],
   imports: [
     BrowserModule,
