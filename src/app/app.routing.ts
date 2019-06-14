0fca316b54abdc677bb112efd5b3fcf26a3f5304
bh <bwhellar@gmail.com>
2019-06-14 14:10:14 -0700

    break the html elements into different html components

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,18 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { CenterComponent } from './center/center.component';
 
 
 const appRoutes: Routes = [
   {
   path: '',
   component: TopBarComponent
+},
+  {
+  path: "",
+  component: CenterComponent,
+  outlet: "center"
   }
 ];
 
